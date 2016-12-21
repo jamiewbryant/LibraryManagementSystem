b90f6a2a02bffed0a5b383261364a22f5db9daa3
jamiewbryant <bryant.jamie.w@gmail.com>
2016-12-21 18:42:17 -0500

    First Commit

--- SQL_Drill.sql
+++ SQL_Drill.sql
@@ -1,3 +1,211 @@
+--------------------Book-------------
+USE [LibraryManagementSystem]
+GO
+
+/****** Object:  Table [Books].[Book]    Script Date: 12/21/2016 6:27:42 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [Books].[Book](
+	[BookID] [int] IDENTITY(1,1) NOT NULL,
+	[Title] [varchar](50) NULL,
+	[PublisherName] [varchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[BookID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+--------------------------Book_Authors-----------------------------
+USE [LibraryManagementSystem]
+GO
+
+/****** Object:  Table [Books].[Book_Authors]    Script Date: 12/21/2016 6:28:59 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [Books].[Book_Authors](
+	[BookID] [int] IDENTITY(1,1) NOT NULL,
+	[AuthorName] [varchar](50) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [Books].[Book_Authors]  WITH CHECK ADD FOREIGN KEY([BookID])
+REFERENCES [Books].[Book] ([BookID])
+GO
+
+-------------------------------Book_Copies-------------------------
+USE [LibraryManagementSystem]
+GO
+
+/****** Object:  Table [Books].[Book_Copies]    Script Date: 12/21/2016 6:29:46 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [Books].[Book_Copies](
+	[BookID] [int] NOT NULL,
+	[BranchID] [int] NOT NULL,
+	[No_Of_Copies] [int] NOT NULL
+) ON [PRIMARY]
+
+GO
+
+ALTER TABLE [Books].[Book_Copies]  WITH CHECK ADD FOREIGN KEY([BookID])
+REFERENCES [Books].[Book] ([BookID])
+GO
+
+ALTER TABLE [Books].[Book_Copies]  WITH CHECK ADD FOREIGN KEY([BranchID])
+REFERENCES [Books].[Library_Branch] ([BranchID])
+GO
+
+---------------------------------------Book_Loans---------------------------
+USE [LibraryManagementSystem]
+GO
+
+/****** Object:  Table [Books].[Book_Loans]    Script Date: 12/21/2016 6:30:20 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [Books].[Book_Loans](
+	[BookID] [int] NOT NULL,
+	[BranchID] [int] NOT NULL,
+	[CardNo] [int] NOT NULL,
+	[DateOut] [varchar](50) NULL,
+	[DueDate] [varchar](50) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+ALTER TABLE [Books].[Book_Loans]  WITH CHECK ADD FOREIGN KEY([BookID])
+REFERENCES [Books].[Book] ([BookID])
+GO
+
+ALTER TABLE [Books].[Book_Loans]  WITH CHECK ADD FOREIGN KEY([BranchID])
+REFERENCES [Books].[Library_Branch] ([BranchID])
+GO
+
+ALTER TABLE [Books].[Book_Loans]  WITH CHECK ADD FOREIGN KEY([CardNo])
+REFERENCES [Books].[Borrower] ([CardNo])
+GO
+
+----------------Borrower-------------------------------------------
+USE [LibraryManagementSystem]
+GO
+
+/****** Object:  Table [Books].[Borrower]    Script Date: 12/21/2016 6:30:49 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [Books].[Borrower](
+	[CardNo] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [varchar](50) NULL,
+	[Address] [varchar](50) NULL,
+	[Phone] [varchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[CardNo] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+----------------------Library_Branch-----------------------------
+USE [LibraryManagementSystem]
+GO
+
+/****** Object:  Table [Books].[Library_Branch]    Script Date: 12/21/2016 6:31:31 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [Books].[Library_Branch](
+	[BranchID] [int] IDENTITY(1,1) NOT NULL,
+	[BranchName] [varchar](50) NULL,
+	[Address] [varchar](50) NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[BranchID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+---------------------Publisher--------------------------------
+USE [LibraryManagementSystem]
+GO
+
+/****** Object:  Table [Books].[Publisher]    Script Date: 12/21/2016 6:32:17 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+SET ANSI_PADDING ON
+GO
+
+CREATE TABLE [Books].[Publisher](
+	[Name] [varchar](50) NULL,
+	[Address] [varchar](50) NULL,
+	[Phone] [varchar](50) NULL
+) ON [PRIMARY]
+
+GO
+
+SET ANSI_PADDING OFF
+GO
+
+
 --Problem1Or2-------------------------------------------------------
 USE [LibraryManagementSystem]
 GO
